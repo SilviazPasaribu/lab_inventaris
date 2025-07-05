0cbff08ff38b11527fa70592d3b15c5a70a8835c
yunidar123 <nidarhulu488@gmail.com>
2025-07-05 21:11:17 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'inventaris_lab_unika',
+  webDir: 'www'
+};
+
+export default config;
